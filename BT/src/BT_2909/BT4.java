e4aa8205dce8386d400798170b723962e7074031
ADMIN <ADMIN@DESKTOP-FO8QLMB>
2021-09-29 14:30:54 +0700

    BT 29/090

--- BT/src/BT_2909/BT4.java
+++ BT/src/BT_2909/BT4.java
@@ -11,13 +11,13 @@ public class BT4 {
     public static String getDayName(int day) {
         String dayName = "";
         switch (day) {
-            case 1: dayName = "Monday"; break;
-            case 2: dayName = "Tuesday"; break;
-            case 3: dayName = "Wednesday"; break;
-            case 4: dayName = "Thursday"; break;
-            case 5: dayName = "Friday"; break;
-            case 6: dayName = "Saturday"; break;
-            case 7: dayName = "Sunday"; break;
+            case 0: dayName = "Monday"; break;
+            case 1: dayName = "Tuesday"; break;
+            case 2: dayName = "Wednesday"; break;
+            case 3: dayName = "Thursday"; break;
+            case 4: dayName = "Friday"; break;
+            case 5: dayName = "Saturday"; break;
+            case 6: dayName = "Sunday"; break;
             default:dayName = "Invalid day range";
         }
         return dayName;
